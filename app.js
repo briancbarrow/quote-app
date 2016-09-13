875b9fbec23f9441191c0d56ecef993d2c78743a
Brian Barrow <briancbarrow@gmail.com>
2016-09-13 07:03:06 -0600

    cleaned up js code

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
-
   var getQuote = function() {
     $.ajax({
       url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?',
       dataType: 'jsonp',
       type: 'GET',
       success: function(data){
-        console.log(data);   
         $('.quote').text(data.quoteText)
         if(data.quoteAuthor === ''){
           $('.author').text('- Anonymous') 
